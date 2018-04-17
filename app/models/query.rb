52816c5e7eaa2af2cb091e48792da5dc0b7cca94
briecodes <hello@brie.work>
2018-04-17 18:02:32 -0400

    Models edited, console.rb added

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,14 +1,14 @@
 class Query < ActiveRecord::Base
   # ActiveRecord
   # has_many :articles, through: :queryresults
-  attr_reader :name
-
-	ALL = []
-
-  def initialize(term_inst)
-		@name == "Search Term: #{term_inst.name}"
-		ALL << self
-	end
+  # attr_reader :name
+  #
+	# ALL = []
+  #
+  # def initialize(term_inst)
+	# 	@name == "Search Term: #{term_inst.name}"
+	# 	ALL << self
+	# end
 
 	def search_term(term_inst)
 		Article.all.select do |article|
--- app/models/term.rb
+++ app/models/term.rb
@@ -1,14 +1,14 @@
 class Term < ActiveRecord::Base
   # ActiveRecord
-  # 
-	attr_reader :term
-
-  ALL = []
-
-	def initialize(term)
-		@term = term
-    ALL << self
-	end
+  #
+	# attr_reader :term
+  #
+  # ALL = []
+  #
+	# def initialize(term)
+	# 	@term = term
+  #   ALL << self
+	# end
 
 	def search_term
 		q = Query.new(self)
