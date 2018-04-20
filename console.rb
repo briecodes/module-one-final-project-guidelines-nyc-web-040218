e0f11e265df64c34c9f6ec6e8eb11829d8362382
briecodes <hello@brie.work>
2018-04-19 21:17:01 -0400

    Sample entry data commented out.

--- console.rb
+++ console.rb
@@ -1,23 +1,23 @@
 require_relative './config/environment.rb'
 require_all 'app'
 
-bones = Term.new(term: "bones")
-graveyard = Term.new(term: "graveyard")
-island = Term.new(term: "island")
-food = Term.new(term: "food")
-
-areax = Article.new(title: "Area X", content: "What lies beyond the border?", url: "http://a.co/aJJk31s")
-anni = Article.new(title:"Annihilation", content:"First book in the trilogy", url: "http://a.co/3TK1fYs")
-auth = Article.new(title:"Authority", content:"Second book in the trilogy", url: "http://a.co/40Jdjeg")
-acc = Article.new(title: "Acceptance", content: "Third book in the trilogy", url: "http://a.co/e5jBHi4")
-
-bones.save
-graveyard.save
-island.save
-food.save
-areax.save
-anni.save
-auth.save
-acc.save
+# bones = Term.new(term: "bones")
+# graveyard = Term.new(term: "graveyard")
+# island = Term.new(term: "island")
+# food = Term.new(term: "food")
+#
+# areax = Article.new(title: "Area X", content: "What lies beyond the border?", url: "http://a.co/aJJk31s")
+# anni = Article.new(title:"Annihilation", content:"First book in the trilogy", url: "http://a.co/3TK1fYs")
+# auth = Article.new(title:"Authority", content:"Second book in the trilogy", url: "http://a.co/40Jdjeg")
+# acc = Article.new(title: "Acceptance", content: "Third book in the trilogy", url: "http://a.co/e5jBHi4")
+#
+# bones.save
+# graveyard.save
+# island.save
+# food.save
+# areax.save
+# anni.save
+# auth.save
+# acc.save
 
 pry.start
