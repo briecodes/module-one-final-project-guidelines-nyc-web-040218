f11f23497d39b5c59b71d202538e9ff472bbda2a
briecodes <hello@brie.work>
2018-04-19 21:31:29 -0400

    Methods commenting and refactoring.

--- app/methods.rb
+++ app/methods.rb
@@ -18,7 +18,7 @@ end
 
 def instruction
   puts ""
-  puts "Please enter your search term, or type 'exit'."
+  puts "Please enter your search term, or type 'EXIT'."
   puts ""
 end
 
@@ -54,33 +54,35 @@ def check_response(word)
 end
 
 def order_results(order_method, results_array)
-  # binding.pry
   if order_method == "authors[0].full_name"
     results_array.sort!{|a, b| a.authors[0].full_name <=> b.authors[0].full_name}
   else
-  # binding.pry
     results_array.sort!{|a, b| b.send(order_method) <=> a.send(order_method)}
   end
 end
 
+# GET PAST SEARCH WORDS AND BOOK ASSSOCIATIONS
 def get_past_searches
   SearchTerm.all.each do |term|
-    puts "'#{term.term}' Resulted in #{term} matches:"
+    puts "'#{term.term}' Resulted in #{term} matche(s):"
   end
 end
 
+# TOP 5 POPULAR BOOKS
 def most_popular
   pop = Book.all.map{|b|b}
-  pop.sort!{|a,b| b.avg_rating <=> a.avg_rating}
+  pop.sort!{|a,b| b.avg_rating <=> a.avg_rating}.take(5)
   puts_results_special(pop)
 end
 
+# TOP 5 MOST REVIEWED BOOKS
 def most_reviews
   pop = Book.all.map{|b|b}
-  pop.sort!{|a,b| b.ratings_count <=> a.ratings_count}
+  pop.sort!{|a,b| b.ratings_count <=> a.ratings_count}.take(5)
   puts_results_special(pop)
 end
 
+# MOST POPULAR WORDS FROM BOOK DESCRIPTIONS
 def popular_words
   words = []
   pop = Book.all.each{|b| words << b.description}
@@ -91,14 +93,12 @@ end
 
 def create_term_instance(word)
   SearchTerm.find_or_create_by({:term => word})
-  # if SearchTerm.all.exists?(:term => "word")
-  # end
 end
 
 def create_query_from_term_instance(term_inst)
   q = Query.new({term_id: term_inst.id})
   q.save
-  q
+  # q
 end
 
 def search_term_instance(term_inst)
@@ -114,6 +114,7 @@ def save_query_results(results_array, query_inst)
   end
 end
 
+# SECRET SAUCE METHOD FOR SPECIAL SEARCHES, PROVIDING LIMITED INFORMATION
 def puts_results_special(results_array)
   if results_array.count > 0
     puts ""
@@ -135,42 +136,48 @@ def puts_results_special(results_array)
   end
 end
 
+def any_search_results?(results_array)
+  results_array.count > 0
+end
+
+def no_matches
+  puts ""
+  puts ""
+  puts "Sorry, no matches for your search. Try again!"
+  puts ""
+end
+
+# METHOD TO PUT RESULTS OF SEARCHES
 def puts_results(results_array)
-  if results_array.count > 0
-    puts ""
-    puts ""
-    puts "We found the following #{results_array.count} results:"
-    puts ""
-    results_array.each do |book|
-      puts "Title: #{book.title}"
-      puts "Publish Date: #{book.pub_date}"
-      if book.page_count
-        puts "#{book.page_count} pages."
-      end
-      puts "Author(s): #{book.authors.map{|a| a.full_name}.join(', ')}"
-      puts "#{book.description}"
-      puts "Categories: #{book.categories.map{|c| c.cat_word}.join(', ')}"
-      if book.avg_rating
-        puts "Average Rating: #{book.avg_rating}, from #{book.ratings_count} responses."
-      end
-      puts "Buy now: #{book.url}"
-      puts ""
-      puts "* * * * * * * * * * * * * * * *"
-      puts ""
+  puts ""
+  puts ""
+  puts "We found the following #{results_array.count} results:"
+  puts ""
+
+  results_array.each do |book|
+    puts "Title: #{book.title}"
+    puts "Publish Date: #{book.pub_date}"
+    if book.page_count
+      puts "#{book.page_count} pages."
     end
-  else
+    puts "Author(s): #{book.authors.map{|a| a.full_name}.join(', ')}"
+    puts "#{book.description}"
+    puts "Categories: #{book.categories.map{|c| c.cat_word}.join(', ')}"
+    if book.avg_rating
+      puts "Average Rating: #{book.avg_rating}, from #{book.ratings_count} responses."
+    end
+    puts "Buy now: #{book.url}"
     puts ""
-    puts ""
-    puts "Sorry, no matches for your search. Try again!"
+    puts "* * * * * * * * * * * * * * * *"
     puts ""
   end
 end
 
+# ABILITY TO ORDER RESULTS
 def order?(results_array)
   order_question
   up_word = get_response
   response = check_response(up_word)
-  # binding.pry
   order_results(response, results_array)
   puts_results(results_array)
   puts ""
@@ -178,7 +185,6 @@ def order?(results_array)
   puts "Filtered by #{up_word.upcase}"
   puts ""
   puts ""
-
   order?(results_array)
 end
 
@@ -198,8 +204,14 @@ def run
   results_array = search_term_instance(new_term)
   # SAVE RESULTS & QUERY RELATIONSHIP TO DATABASE
   save_query_results(results_array, new_query)
+  # CHECK IF ANY RESULTS
+  if any_search_results?(results_array)
+    puts_results(results_array)
+  else
+    no_matches
+    run
+  end
   # FORMATS & PUTS RESULTS TO THE SCREEN
-  puts_results(results_array)
 
   puts "YOUR BREADCRUMBS… #{MY_WORDS.join(" > ")}"
   puts ""
