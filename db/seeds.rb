5e993052a1b74eadf71c6eed3c31a56a0e577ef3
briecodes <hello@brie.work>
2018-04-19 21:14:04 -0400

    Seeds.rb > superseed method corrected. Database rebuilt.

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,6 +95,7 @@ def superseed(seed_arr)
 
     # PARSING RESULTS
     @result["items"].each do |volume|
+
     # BOOK CREATION
       new_book = Book.new()
       new_book.title = volume["volumeInfo"]["title"]
@@ -102,19 +103,27 @@ def superseed(seed_arr)
       volume["volumeInfo"]["description"] ? new_book.description = volume["volumeInfo"]["description"] : new_book.description = "No description available."
       new_book.page_count = volume["volumeInfo"]["pageCount"]
       new_book.url = volume["volumeInfo"]["infoLink"]
-      new_book.avg_rating = volume["volumeInfo"]["averageRating"]
-      new_book.ratings_count = volume["volumeInfo"]["ratingsCount"]
+      if volume["volumeInfo"]["averageRating"]
+        new_book.avg_rating = volume["volumeInfo"]["averageRating"]
+      else
+        new_book.avg_rating = 0
+      end
+      if volume["volumeInfo"]["ratingsCount"]
+        new_book.ratings_count = volume["volumeInfo"]["ratingsCount"]
+      else
+        new_book.ratings_count = 0
+      end
       new_book.save
 
     # AUTHOR CREATION
       if volume["volumeInfo"]["authors"]
         volume["volumeInfo"]["authors"].each do |name|
-          new_author = Author.find_or_create_by({:author => name})
+          new_author = Author.find_or_create_by({:full_name => name})
           new_ba = BookAuthor.new({book_id: new_book.id, author_id: new_author.id})
           new_ba.save
         end
       else
-        new_author = Author.find_or_create_by({:author => "Unknown"})
+        new_author = Author.find_or_create_by({:full_name => "Unknown"})
         new_ba = BookAuthor.new({book_id: new_book.id, author_id: new_author.id})
         new_ba.save
       end
@@ -122,17 +131,16 @@ def superseed(seed_arr)
     # CATEGORY CREATION
       if volume["volumeInfo"]["categories"]
         volume["volumeInfo"]["categories"].each do |cat|
-          new_cat = Category.find_or_create_by({:category => cat})
+          new_cat = Category.find_or_create_by({:cat_word => cat})
           new_bc = BookCategory.new({book_id: new_book.id, category_id: new_cat.id})
           new_bc.save
         end
       else
-        new_cat = Category.find_or_create_by({:category => "Unknown"})
+        new_cat = Category.find_or_create_by({:cat_word => "Unknown"})
         new_bc = BookCategory.new({book_id: new_book.id, category_id: new_cat.id})
         new_bc.save
       end
     end
-
     # OUTCOME CONFIRMATION
     puts "You have #{Book.all.count} books in your DB."
 
