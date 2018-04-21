2b3d7775e3929b137e6723f9ba108a890395dd3b
briecodes <hello@brie.work>
2018-04-21 15:14:04 -0400

    Book class edited to contain self.most_popular.

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,8 +8,8 @@ class Book < ActiveRecord::Base
   has_many :search_terms, through: :queries
 
   def self.most_popular
-    pop = self.all
-    pop = pop.sort{|a,b| b.avg_rating <=> a.avg_rating}
+    # pop = self.all
+    pop = self.all.sort{|a,b| b.avg_rating <=> a.avg_rating}
     puts_results_special(pop.take(5))
   end
 
